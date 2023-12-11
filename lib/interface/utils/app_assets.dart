41e2e182aec089d28851640734c65f46ad6a4434
Mohamed <mohamed.osama200538@gmail.com>
2023-12-11 14:53:19 +0200

    update project structure

---
+++ lib/interface/utils/app_assets.dart
@@ -0,0 +1,12 @@
+abstract class AppAssets {
+  static const String baseImagesPath = "assets/images/";
+  static const String splashLogo = "${baseImagesPath}splash.png";
+  static const String iconBrows = "${baseImagesPath}Icon_brows.svg";
+  static const String iconWatchList = "${baseImagesPath}Icon_watch_list.svg";
+  static const String imageTest = "${baseImagesPath}test.png";
+  static const String movieBackGround = "${baseImagesPath}background.png";
+  static const String notFoundImage =
+      "https://www.joshuahimalayas.org/public/data_not_found.png";
+  static const String searshNotFound =
+      "${baseImagesPath}Icon material-local-movies.png";
+}
---
+++ lib/interface/utils/strings.dart
@@ -0,0 +1,6 @@
+abstract class CommonStrings {
+  static const String baseApiUrl = "https://api.themoviedb.org/3/movie/";
+  static const String token =
+      "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4ZjY0ZTg1ZjEwOGEzMGM3MjlmMThhMzQ2MGEyOWUzNyIsInN1YiI6IjY1NjQ3ZmRjYTZjMTA0MDEzODJiMDg0YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.5PQTdswGr3k0OQfA6A-FOn_KuH9ndq57UQQziQlmrRA";
+  static const String apiKey = "8f64e85f108a30c729f18a3460a29e37";
+}
